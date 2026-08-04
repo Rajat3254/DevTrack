c4e92f3064602d4a44bfac08be36145ff93adf0f
Rajat Singh <singhr3234@gmail.com>
2026-08-04 18:30:21 +0530

    message

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  return (
+    <h1 className="text-4xl font-bold text-center mt-10">
+      DevTrack
+    </h1>
+  );
+}
+
+export default App;
